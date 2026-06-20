e25ba7e9f1643c0c77d26f565e2813974f8441a3
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-06-21 00:51:18 +0800

    feat: setting up SQLite

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,9 +6,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Available for valdation for data annotation
 builder.Services.AddValidation();
 
+// Add DB context (before app)
+
+// command: dotnet ef migrations add {migrationMessage} --output-dir Data/Migrations
+
+// command: dotnet ef database update
+// note: replaced by `.MigrateDb()` code, which read the migration file and do something like init/update db
+
+// ps: stop all ongoing process before doing migration
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3015be9f6723931aaa54e87dce12468d44ff5490")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
